b2517897c683e2ffc24c701b69179af6d8952d21
devops-shaka <devops@shaka.website>
2023-03-28 16:55:30 +0000

    feat(web): develops home page, prototypes for fundraise page lightning donation feature

--- apps/web/src/features/_shaka/ShakaOrigin.tsx
+++ apps/web/src/features/_shaka/ShakaOrigin.tsx
@@ -50,14 +50,62 @@ export const ShakaOrigin: React.FC<TypesShakaOrigin> = ({
           </div>
         </div>
 
-        <div className={`flex flex-row px-8 py-16 justify-center`}>
-          <Link href={`/fundraise`}>
-            <p
-              className={`font-apercu font-medium text-3xl text-white hover:text-opacity-70 pointer-cursor`}
-            >
-              {`${t(`glossary:join_our_crowdfund`, `join_our_crowdfund`)}`}
-            </p>
-          </Link>
+        <div className={`flex flex-col w-full pt-24 space-y-16`}>
+          <div className={`group flex flex-row px-8`}>
+            <div className={`flex flex-row items-center space-x-4`}>
+              <Link href={`/fundraise`}>
+                <p
+                  className={`font-apercu font-medium text-3xl text-white group-hover:text-opacity-70 `}
+                >
+                  {`${t(`glossary:join_our_crowdfund`, `join_our_crowdfund`)}`}
+                </p>
+              </Link>
+              <div className={`flex text-white`}>
+                <svg
+                  xmlns={"http://www.w3.org/2000/svg"}
+                  viewBox={"0 0 24 24"}
+                  fill={"currentColor"}
+                  className={"w-6 h-6"}
+                >
+                  <path
+                    fillRule={"evenodd"}
+                    d={
+                      "M14.615 1.595a.75.75 0 01.359.852L12.982 9.75h7.268a.75.75 0 01.548 1.262l-10.5 11.25a.75.75 0 01-1.272-.71l1.992-7.302H3.75a.75.75 0 01-.548-1.262l10.5-11.25a.75.75 0 01.913-.143z"
+                    }
+                    clipRule={"evenodd"}
+                  />
+                </svg>
+              </div>
+            </div>
+          </div>
+
+          <div className={`group flex flex-row px-8 `}>
+            <div className={`flex flex-row items-center space-x-4 `}>
+              <Link href={`/contact`}>
+                <p
+                  className={`font-apercu font-medium text-3xl text-white group-hover:text-opacity-70`}
+                >
+                  {`${t(`glossary:`, `Get in touch`)}`}
+                </p>
+              </Link>
+              <div className={`flex text-white`}>
+                <svg
+                  xmlns={"http://www.w3.org/2000/svg"}
+                  viewBox={"0 0 24 24"}
+                  fill={"currentColor"}
+                  className={"w-6 h-6"}
+                >
+                  <path
+                    fillRule={"evenodd"}
+                    d={
+                      "M5.337 21.718a6.707 6.707 0 01-.533-.074.75.75 0 01-.44-1.223 3.73 3.73 0 00.814-1.686c.023-.115-.022-.317-.254-.543C3.274 16.587 2.25 14.41 2.25 12c0-5.03 4.428-9 9.75-9s9.75 3.97 9.75 9c0 5.03-4.428 9-9.75 9-.833 0-1.643-.097-2.417-.279a6.721 6.721 0 01-4.246.997z"
+                    }
+                    clipRule={"evenodd"}
+                  />
+                </svg>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
       <ShakaFooter basis={{ ...basis }} />
--- apps/web/src/features/fundraise/ShakaFundraiseOrigin.tsx
+++ apps/web/src/features/fundraise/ShakaFundraiseOrigin.tsx
@@ -140,22 +140,56 @@ export const ShakaFundraiseOrigin: React.FC<TypesShakaFundraiseOrigin> = ({
           }}
         />
 
-        <div className={`flex flex-col w-full space-y-2`}>
-          <div className={`flex flex-row w-full h-12 justify-center`}>
+        <div className={`flex flex-col w-full space-y-2 pb-24`}>
+          <div
+            className={`flex flex-row w-full items-center justify-center space-x-4 pt-4`}
+          >
+            <div className={`flex text-white`}>
+              <svg
+                xmlns={"http://www.w3.org/2000/svg"}
+                viewBox={"0 0 24 24"}
+                fill={"currentColor"}
+                className={"w-6 h-6"}
+              >
+                <path
+                  fillRule={"evenodd"}
+                  d={
+                    "M14.615 1.595a.75.75 0 01.359.852L12.982 9.75h7.268a.75.75 0 01.548 1.262l-10.5 11.25a.75.75 0 01-1.272-.71l1.992-7.302H3.75a.75.75 0 01-.548-1.262l10.5-11.25a.75.75 0 01.913-.143z"
+                  }
+                  clipRule={"evenodd"}
+                />
+              </svg>
+            </div>
             <p
-              className={`font-apercu font-medium text-xl text-white font-bold `}
+              className={`font-apercu font-medium text-3xl text-white font-bold `}
             >
               {`Lightning Donation`}
             </p>
+            <div className={`flex text-white`}>
+              <svg
+                xmlns={"http://www.w3.org/2000/svg"}
+                viewBox={"0 0 24 24"}
+                fill={"currentColor"}
+                className={"w-6 h-6"}
+              >
+                <path
+                  fillRule={"evenodd"}
+                  d={
+                    "M14.615 1.595a.75.75 0 01.359.852L12.982 9.75h7.268a.75.75 0 01.548 1.262l-10.5 11.25a.75.75 0 01-1.272-.71l1.992-7.302H3.75a.75.75 0 01-.548-1.262l10.5-11.25a.75.75 0 01.913-.143z"
+                  }
+                  clipRule={"evenodd"}
+                />
+              </svg>
+            </div>
           </div>
-          <div className={`flex flex-col w-full px-8 `}>
+          <div className={`flex flex-col w-full pt-8 px-8 items-center  `}>
             <div
-              className={`flex flex-col rounded min-h-120 py-6 px-4 space-y-3 bg-[#FDFCFE]`}
+              className={`flex flex-col rounded min-h-120 lg:w-120 py-6 px-4 space-y-3 bg-[#FDFCFE]`}
             >
               <div
                 className={`flex flex-col rounded w-full h-36 px-4 bg-secondary opacity-80 justify-center items-center`}
               >
-                <div className={`flex flex-row h-20 `}>
+                <div className={`flex flex-row h-20 flex-1`}>
                   <div
                     className={`flex w-12 h-full items-center justify-center`}
                   >
@@ -182,14 +216,16 @@ export const ShakaFundraiseOrigin: React.FC<TypesShakaFundraiseOrigin> = ({
                     )}
                   </div>
                   <div
-                    className={`flex flex-row h-full items-center justify-center`}
+                    className={`flex flex-row w-full h-full items-center justify-center`}
                   >
                     <input
                       type={"text"}
                       placeholder={
-                        FundraiseShape.moneykind === `btc` ? `1000000` : `10`
+                        FundraiseShape.moneykind === `btc` ? `40,000` : `10`
                       }
-                      className={`input input-ghost w-[190px] font-apercu font-medium text-4xl placeholder:text-white placeholder:text-start text-white focus:bg-transparent focus:outline-0`}
+                      className={`input input-ghost w-[${
+                        FundraiseShape.moneykind === `btc` ? `170px` : `80px`
+                      }] font-apercu font-medium text-4xl placeholder:text-white placeholder:text-start text-white focus:bg-transparent focus:outline-0`}
                     />
                   </div>
                 </div>
@@ -227,14 +263,16 @@ export const ShakaFundraiseOrigin: React.FC<TypesShakaFundraiseOrigin> = ({
 
               {FundraiseShape.lninvoice ? (
                 <>
-                  <ShakaQr
-                    basis={{
-                      ...basis,
-                      data: FundraiseShape.lninvoice,
-                      size: 300,
-                      cl: `text-secondary-content`,
-                    }}
-                  />
+                  <div className={`flex flex-row w-full justify-center`}>
+                    <ShakaQr
+                      basis={{
+                        ...basis,
+                        data: FundraiseShape.lninvoice,
+                        size: 300,
+                        cl: `text-secondary-content`,
+                      }}
+                    />
+                  </div>
                 </>
               ) : null}
             </div>
@@ -242,6 +280,7 @@ export const ShakaFundraiseOrigin: React.FC<TypesShakaFundraiseOrigin> = ({
         </div>
       </div>
       <ShakaFooter basis={{ ...basis, bg: `bg-secondary` }} />
+      <div className={"w-[80px] w-[170px]"} />
     </>
   );
 };
